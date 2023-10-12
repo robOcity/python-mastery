6ce1e04ddd60b64640501028d8841f79ecbe6256
Rob Osterburg <robert.osterburg@gmail.com>
2023-10-12 16:14:41 -0600

    fix typing and imports

--- memory_use_by_datastructure.py
+++ memory_use_by_datastructure.py
@@ -19,141 +19,20 @@ DICTIONARY:  Memory: 220.5 MB  Peak: 220.6 MB  Instances: 577,563  Object Size: 
 
 import csv
 import math
-from collections import namedtuple
 from multiprocessing import Process, Queue
-from dataclasses import dataclass
-
-# from typing import List, Tuple, Dict, NamedTuple, Union, Callable
-from read_bus_rides import read_rides
-
-
-class Row:
-    """A row (tuple) of CTA bus data using a plain-python class.
-
-    Attributes:
-        route:   Column 1. The bus route ID number
-        date:    Column 2. A date string (MM/DD/YYYY)
-        daytype: Column 3. (U=Sunday/Holiday, A=Saturday, W=Weekday)
-        rides:   Column 4. Total number of rides that day (integer)
-    """
-
-    def __init__(self, row) -> None:
-        self.route = row[0]
-        self.date = row[1]
-        self.daytype = row[2]
-        self.rides = row[3]
-
-
-class SlotsRow:
-    """A row (tuple) of CTA bus data using a python class with slots.
-
-    Attributes:
-        route:   Column 1. The bus route ID number
-        date:    Column 2. A date string (MM/DD/YYYY)
-        daytype: Column 3. (U=Sunday/Holiday, A=Saturday, W=Weekday)
-        rides:   Column 4. Total number of rides that day (integer)
-    """
-
-    __slots__ = ["route", "date", "daytype", "rides"]
-
-    def __init__(self, row) -> None:
-        self.route = row[0]
-        self.date = row[1]
-        self.daytype = row[2]
-        self.rides = row[3]
-
-
-@dataclass
-class DataclassRow:
-    """A row (tuple) of CTA bus data stored in a dataclass.
-
-    Attributes:
-        route:   Column 1. The bus route ID number
-        date:    Column 2. A date string (MM/DD/YYYY)
-        daytype: Column 3. (U=Sunday/Holiday, A=Saturday, W=Weekday)
-        rides:   Column 4. Total number of rides that day (integer)
-    """
-
-    route: str
-    date: str
-    daytype: str
-    rides: str
-
-
-DataCollection = List[Union[Dict, Tuple, NamedTuple, Row, SlotsRow, DataclassRow]]
-
-
-def build_tuple(row: List[str]) -> Tuple[str, str, str, str]:
-    """Constructs a tuple containing row of bus data.
-    Args:
-        A row (tuple) of CTA bus data.
-
-    Return:
-        A tuple("route", "date", "daytype", "rides").
-    """
-    route = row[0]
-    date = row[1]
-    daytype = row[2]
-    rides = row[3]
-    return (route, date, daytype, rides)
-
-
-def build_dictionary(row: List[str]) -> Dict[str, str]:
-    """Constructs a dictionary containing row of bus data.
-    Args:
-        A row (tuple) of CTA bus data.
-
-    Return:
-        A dict containing the route, date, daytype, and rides.
-    """
-    return {"route": row[0], "date": row[1], "daytype": row[2], "rides": row[3]}
-
-
-NamedtupleRow = namedtuple("NamedtupleRow", ["route", "date", "daytype", "rides"])
-
-
-def build_namedtuple(row: List[str]) -> NamedtupleRow:
-    """Constructs a namedtuple containing row of bus data.
-    Args:
-        A row (tuple) of CTA bus data.
-
-    Return:
-        A namedtuple containing the route, date, daytype, and rides.
-    """
-    return NamedtupleRow(row[0], row[1], row[2], row[3])
-
-
-def build_class(row: List[str]) -> Row:
-    """Constructs a class instance representing a row of bus data.
-    Args:
-        A row (tuple) of CTA bus data.
-
-    Return:
-        A class containing the route, date, daytype, and rides attributes.
-    """
-    return Row(row)
-
-
-def build_slots(row: List[str]) -> SlotsRow:
-    """Constructs a memory efficient class instance representing a row of bus data.
-    Args:
-        A row (tuple) of CTA bus data.
-
-    Return:
-        A memory efficient class containing the route, date, daytype, and rides attributes.
-    """
-    return SlotsRow(row)
-
-
-def build_dataclass(row: List[str]) -> DataclassRow:
-    """Constructs a dataclass instance representing a row of bus data.
-    Args:
-        A row (tuple) of CTA bus data.
-
-    Return:
-        A dataclass containing the route, date, daytype, and rides attributes.
-    """
-    return DataclassRow(row[0], row[1], row[2], row[3])
+from typing import Callable, List
+from read_bus_rides import (
+    Row,
+    DataclassRow,
+    SlotsRow,
+    read_rides,
+    build_class,
+    build_dataclass,
+    build_dictionary,
+    build_namedtuple,
+    build_slots,
+    build_tuple,
+)
 
 
 def measure_memory(filename: str, func: Callable, queue: Queue) -> None:
--- read_bus_rides.py
+++ read_bus_rides.py
@@ -1,6 +1,138 @@
 # read_bus_rides.py
 
 import csv
+from collections import namedtuple
+from dataclasses import dataclass
+from typing import Callable, List, Tuple, Dict, NamedTuple, Union, Callable
+
+
+class Row:
+    """A row (tuple) of CTA bus data using a plain-python class.
+
+    Attributes:
+        route:   Column 1. The bus route ID number
+        date:    Column 2. A date string (MM/DD/YYYY)
+        daytype: Column 3. (U=Sunday/Holiday, A=Saturday, W=Weekday)
+        rides:   Column 4. Total number of rides that day (integer)
+    """
+
+    def __init__(self, row) -> None:
+        self.route = row[0]
+        self.date = row[1]
+        self.daytype = row[2]
+        self.rides = row[3]
+
+
+class SlotsRow:
+    """A row (tuple) of CTA bus data using a python class with slots.
+
+    Attributes:
+        route:   Column 1. The bus route ID number
+        date:    Column 2. A date string (MM/DD/YYYY)
+        daytype: Column 3. (U=Sunday/Holiday, A=Saturday, W=Weekday)
+        rides:   Column 4. Total number of rides that day (integer)
+    """
+
+    __slots__ = ["route", "date", "daytype", "rides"]
+
+    def __init__(self, row) -> None:
+        self.route = row[0]
+        self.date = row[1]
+        self.daytype = row[2]
+        self.rides = row[3]
+
+
+@dataclass
+class DataclassRow:
+    """A row (tuple) of CTA bus data stored in a dataclass.
+
+    Attributes:
+        route:   Column 1. The bus route ID number
+        date:    Column 2. A date string (MM/DD/YYYY)
+        daytype: Column 3. (U=Sunday/Holiday, A=Saturday, W=Weekday)
+        rides:   Column 4. Total number of rides that day (integer)
+    """
+
+    route: str
+    date: str
+    daytype: str
+    rides: str
+
+
+DataCollection = List[Union[Dict, Tuple, NamedTuple, Row, SlotsRow, DataclassRow]]
+
+
+def build_tuple(row: List[str]) -> Tuple[str, str, str, str]:
+    """Constructs a tuple containing row of bus data.
+    Args:
+        A row (tuple) of CTA bus data.
+
+    Return:
+        A tuple("route", "date", "daytype", "rides").
+    """
+    route = row[0]
+    date = row[1]
+    daytype = row[2]
+    rides = row[3]
+    return (route, date, daytype, rides)
+
+
+def build_dictionary(row: List[str]) -> Dict[str, str]:
+    """Constructs a dictionary containing row of bus data.
+    Args:
+        A row (tuple) of CTA bus data.
+
+    Return:
+        A dict containing the route, date, daytype, and rides.
+    """
+    return {"route": row[0], "date": row[1], "daytype": row[2], "rides": row[3]}
+
+
+NamedtupleRow = namedtuple("NamedtupleRow", ["route", "date", "daytype", "rides"])
+
+
+def build_namedtuple(row: List[str]) -> NamedtupleRow:
+    """Constructs a namedtuple containing row of bus data.
+    Args:
+        A row (tuple) of CTA bus data.
+
+    Return:
+        A namedtuple containing the route, date, daytype, and rides.
+    """
+    return NamedtupleRow(row[0], row[1], row[2], row[3])
+
+
+def build_class(row: List[str]) -> Row:
+    """Constructs a class instance representing a row of bus data.
+    Args:
+        A row (tuple) of CTA bus data.
+
+    Return:
+        A class containing the route, date, daytype, and rides attributes.
+    """
+    return Row(row)
+
+
+def build_slots(row: List[str]) -> SlotsRow:
+    """Constructs a memory efficient class instance representing a row of bus data.
+    Args:
+        A row (tuple) of CTA bus data.
+
+    Return:
+        A memory efficient class containing the route, date, daytype, and rides attributes.
+    """
+    return SlotsRow(row)
+
+
+def build_dataclass(row: List[str]) -> DataclassRow:
+    """Constructs a dataclass instance representing a row of bus data.
+    Args:
+        A row (tuple) of CTA bus data.
+
+    Return:
+        A dataclass containing the route, date, daytype, and rides attributes.
+    """
+    return DataclassRow(row[0], row[1], row[2], row[3])
 
 
 def read_rides(filename: str, func: Callable) -> DataCollection:
